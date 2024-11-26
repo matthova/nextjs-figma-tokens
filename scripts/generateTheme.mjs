d0aa33362d706093bc8468f5b96834f6463cdf16
Matt Hova <matthova87@gmail.com>
2024-11-25 22:00:06 -0600

    light and dark themed

---
+++ scripts/generateTheme.mjs
@@ -0,0 +1,72 @@
+import fs from 'fs';
+import { join } from 'path';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import theme from '../theme.json' with { type: 'json'};
+
+function writeThemeToGlobalCss() {
+  const lightTheme = Object.entries(theme.themes.lighttheme).map(([name, value]) => `  --${name}: ${value.value};`)
+  const darkTheme = Object.entries(theme.themes.darktheme).map(([name, value]) => `  --${name}: ${value.value};`);
+  let globalCss = `@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+:root {
+${lightTheme.join('\n')}
+}
+
+@media (prefers-color-scheme: dark) {
+  :root {
+  ${darkTheme.join('\n  ')}
+  }
+}
+
+body {
+  color: var(--onbackground);
+  background: var(--background);
+  font-family: Arial, Helvetica, sans-serif;
+}
+`
+
+  fs.writeFile(join(__dirname, '../src/app/globals.css'), globalCss, (...res) => { console.log('all done', res) });
+}
+
+const REGEX_START = "      // <<< THEME INJECTION START >>>";
+const REGEX_END = "      // <<< THEME INJECTION END >>>";
+function replaceContent(fileContent, replaceString) {
+  const regex = new RegExp(`${REGEX_START}(.*?)${REGEX_END}`, "gs");
+  return fileContent.replace(regex, `${REGEX_START}${replaceString}${REGEX_END}`);
+}
+
+function writeThemeToTailwindConfig(filePath) {
+  // Read the file content
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading the file:', err);
+      return;
+    }
+    const replaceString = `
+      colors: {
+${Object.keys(theme.themes.lighttheme).map((key) => `        ${key}: 'var(--${key})',`).join('\n')}
+      },
+`
+    // Replace the content
+    const modifiedData = replaceContent(data, replaceString);
+
+    // Write the modified content back to the file
+    fs.writeFile(filePath, modifiedData, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing the file:', err);
+        return;
+      }
+      console.log('File has been updated successfully.');
+    });
+  });
+}
+
+writeThemeToGlobalCss();
+
+const tailwindConfigPath = join(__dirname, '../tailwind.config.ts')
+writeThemeToTailwindConfig(tailwindConfigPath);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
   content: [
@@ -6,13 +7,37 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  corePlugins: {
+    divideStyle: true,
+  },
   theme: {
     extend: {
+      // <<< THEME INJECTION START >>>
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        onbackground: 'var(--onbackground)',
+        surface: 'var(--surface)',
+        onsurface: 'var(--onsurface)',
+        primary: 'var(--primary)',
+        primaryhover: 'var(--primaryhover)',
+        primaryactive: 'var(--primaryactive)',
+        link: 'var(--link)',
+        linkhover: 'var(--linkhover)',
+        linkactive: 'var(--linkactive)',
+        error: 'var(--error)',
+        onerror: 'var(--onerror)',
+        warning: 'var(--warning)',
+        onwarning: 'var(--onwarning)',
+        success: 'var(--success)',
+        onsuccess: 'var(--onsuccess)',
+        onprimary: 'var(--onprimary)',
       },
+      // <<< THEME INJECTION END >>>
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('hocus', ['&:hover', '&:focus'])
+    }),
+  ],
 } satisfies Config;
